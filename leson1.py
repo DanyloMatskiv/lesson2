16fe2208b6f9a116a0c414ac2930594672e7f72d
DanyloMatskiv <matskiv0601@gmail.com>
2024-12-01 06:41:33 -0800

    Add files via upload

---
+++ leson1.py
@@ -0,0 +1,113 @@
+# from ipaddress import collapse_addresses
+
+
+# print('Hello World')
+
+# # Тип зміної float нецілі числа
+# Price = 9.99
+
+# # Тип зміної int цілі числа
+# number = 10
+
+# # Тип зміної String рядок або текст
+# name = 'Danylo'
+
+# # Тип зміної bool (boolsed) 
+# light =True
+# # 
+
+
+
+
+# fanta = 1.25
+# cola = 0.5
+
+# year = 13
+# old = 3
+
+
+# surname = 'I'
+# name = 'Malpha'
+
+
+# fan = False
+# night =True
+
+
+
+# print(fanta)
+# print(cola)
+# print(year)
+# print(name)
+# print(old)
+# print(surname)
+# print(fan)
+# print(night)
+
+
+
+
+# print(type(fanta))
+# print(type(cola))
+# print(type(year))
+# print(type(old))
+# print(type(surname))
+# print(type(name))
+# print(type(fan))
+# print(type(night))
+
+
+
+
+# cola =1.50
+# fanta = 0.750
+
+# if cola > 1:
+#     print("You won't get druk")
+# elif fanta > 1:
+#     print("You won't get druk")
+# else:
+#      print("you got drunk")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range (10):
+#     print('Hello World')
+
+
+# a =5
+
+
+# while a < 10:
+#     a += 1
+#     print(a)
+
+
+
+
+
+
+
+for i in range (12):
+    print('Hi')
+
+
+
+
+
+# a = 10
+
+# while a < 20:
+#     a += 1
+#     print(a)
